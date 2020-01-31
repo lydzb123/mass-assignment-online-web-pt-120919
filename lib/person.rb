04d789ecf9d1a1b52142b344aad57ad25f3d67be
Lydia B <lydiabaik@gmail.com>
2020-01-31 04:36:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,9 +4,9 @@ class Person
 :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize
+    
   end
   
 end
 
 
-Your initialize method should use iteration and the .send method to mass assign the value of each key/value pair to its associated key (i.e. method). Refer to the previous reading to help you solve this one.
